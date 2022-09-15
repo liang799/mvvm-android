8ab020df77914fda280c24b3e92f6f439887b18c
liang799 <71926348+liang799@users.noreply.github.com>
2022-09-15 20:41:01 +0800

    Added viewmodel file

---
+++ app/src/main/java/codingflow/com/ui/NoteViewModel.java
@@ -0,0 +1,40 @@
+package codingflow.com.ui;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+import java.util.prefs.NodeChangeEvent;
+
+import codingflow.com.data.repo.NoteRepository;
+import codingflow.com.data.room.Note;
+
+public class NoteViewModel extends AndroidViewModel {
+    private NoteRepository repository;
+    private LiveData<List<Note>> allNotes;
+
+    public NoteViewModel(@NonNull Application application) {
+        super(application);
+        repository = new NoteRepository(application);
+        allNotes = repository.getAllNotes();
+    }
+
+    public void insert(Note note) {
+        repository.insert(note);
+    }
+
+    public void delete(Note note) {
+        repository.delete(note);
+    }
+
+    public void deleteAllNotes() {
+        repository.deleteAllNotes();
+    }
+
+    public LiveData<List<Note>> getAllNotes() {
+        return allNotes;
+    }
+}
