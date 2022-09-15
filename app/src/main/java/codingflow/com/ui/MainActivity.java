b05107cb369f12aecbbbb6e7efd0a3e42c708ca6
liang799 <71926348+liang799@users.noreply.github.com>
2022-09-15 20:41:17 +0800

    Integrating everything...

--- app/src/main/java/codingflow/com/ui/MainActivity.java
+++ app/src/main/java/codingflow/com/ui/MainActivity.java
@@ -7,10 +7,13 @@ import android.os.Bundle;
 import codingflow.com.R;
 
 public class MainActivity extends AppCompatActivity {
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        noteViewModel = ViewModelProviders.of(this).
     }
 }
